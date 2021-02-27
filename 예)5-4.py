845017beb89731abf8c4ed8c193e1e0d8b4a0ed5
Eunju Lee <76032774+Eeun-ju@users.noreply.github.com>
2021-02-27 19:56:26 +0900

    재귀 함수 종료 예제

---
+++ 예)5-4.py
@@ -0,0 +1,10 @@
+def recursive_function(i):
+
+    if i == 10:
+        return
+
+    print(i, '번째 재귀 함수에서', i+1,'번째 재귀 함수를 호출합니다.')
+    recursive_function(i+1)
+    print(i,'번째 재귀 함수를 종료합니다.')
+
+recursive_function(1)
